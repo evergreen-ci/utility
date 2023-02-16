cf4ab8b81733a5dab943ac8b7a02ee1873b10f90
bynn <bynn.lee@mongodb.com>
2023-02-16 15:50:22 -0500

    get rid of gimlet to erase all grip dependencies

--- http.go
+++ http.go
@@ -3,8 +3,6 @@ package utility
 import (
 	"context"
 	"crypto/tls"
-	"encoding/json"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"net/url"
@@ -12,7 +10,6 @@ import (
 	"time"
 
 	"github.com/PuerkitoBio/rehttp"
-	"github.com/evergreen-ci/gimlet"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
 )
@@ -286,33 +283,6 @@ func IsTemporaryError(err error) bool {
 		return terr.Temporary()
 	}
 	return false
-}
-
-// RespErrorf attempts to read a gimlet.ErrorResponse from the response body
-// JSON. If successful, it returns the gimlet.ErrorResponse wrapped with the
-// HTTP status code and the formatted error message. Otherwise, it returns an
-// error message with the HTTP status and raw response body.
-func RespErrorf(resp *http.Response, format string, args ...interface{}) error {
-	if resp == nil {
-		return errors.Errorf(format, args...)
-	}
-	wrapError := func(err error) error {
-		err = errors.Wrapf(err, "HTTP status code %d", resp.StatusCode)
-		return errors.Wrapf(err, format, args...)
-	}
-
-	defer resp.Body.Close()
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return wrapError(errors.Wrap(err, "reading response body"))
-	}
-
-	respErr := gimlet.ErrorResponse{}
-	if err = json.Unmarshal(b, &respErr); err != nil {
-		return wrapError(errors.Errorf("received response: %s", string(b)))
-	}
-
-	return wrapError(respErr)
 }
 
 // RetryRequest takes an http.Request and makes the request until it's successful,
