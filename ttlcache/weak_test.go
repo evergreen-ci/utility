daffdaeab9e0266de14f80bbc8f144a8b960990a
Zackary Santana <64446617+ZackarySantana@users.noreply.github.com>
2025-04-24 09:49:40 -0400

    fix: make sure it releases memory

--- ttlcache/weak_test.go
+++ ttlcache/weak_test.go
@@ -1,11 +1,46 @@
 package ttlcache
 
 import (
+	"runtime"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestTTLWeakInMemoryCache(t *testing.T) {
 	testCache(t, func() Cache[*int] {
 		return NewWeakInMemory[int]()
+	})
+
+	t.Run("RelasesMemory", func(t *testing.T) {
+		getItem := func() *string {
+			i := "itemValue"
+			return &i
+		}
+
+		cache := NewWeakInMemory[string]()
+
+		itemVar := getItem()
+		cache.Put(t.Context(), "id", itemVar, time.Now().Add(time.Hour))
+
+		cachedItem, found := cache.Get(t.Context(), "id", time.Minute)
+		require.True(t, found)
+		require.NotNil(t, cachedItem)
+		assert.Equal(t, itemVar, cachedItem)
+
+		// There are still references to 'itemVar' in the test, so it should not be garbage collected.
+		runtime.GC()
+		cachedItem, found = cache.Get(t.Context(), "id", time.Minute)
+		require.True(t, found)
+		require.NotNil(t, cachedItem)
+		assert.Equal(t, itemVar, cachedItem)
+
+		// There are no references to 'itemVar' in the test, so it should be garbage collected.
+		runtime.GC()
+		cachedItem, found = cache.Get(t.Context(), "id", time.Minute)
+		assert.False(t, found)
+		assert.Nil(t, cachedItem)
 	})
 }
