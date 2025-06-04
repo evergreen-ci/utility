c1b32de37d48fcabe7ea7846cc2e9c218fc20426
Kim Tao <kim.tao@mongodb.com>
2025-06-04 13:37:29 -0400

    DEVPROD-16412: add method to append attributes to context (#80)

--- otel.go
+++ otel.go
@@ -34,10 +34,20 @@ func (processor *attributeSpanProcessor) Shutdown(context.Context) error { retur
 // ForceFlush is a noop to satisfy the SpanProcessor interface.
 func (processor *attributeSpanProcessor) ForceFlush(context.Context) error { return nil }
 
-// ContextWithAttributes returns a child of the ctx containing the attributes. All spans
-// created with the returned context and its children will have the attributes set.
+// ContextWithAttributes returns a child of the ctx containing the attributes.
+// All spans created with the returned context and its children will have the
+// attributes set. If there are any existing attributes on the context already,
+// the returned child context will override all of the existing attributes.
 func ContextWithAttributes(ctx context.Context, attributes []attribute.KeyValue) context.Context {
 	return context.WithValue(ctx, otelAttributeContextKey, attributes)
+}
+
+// ContextWithAppendedAttributes is the same as ContextWithAttributes but
+// creates a child context that appends the provided attributes to the existing
+// attributes in the context (if there are any) rather than overriding them.
+func ContextWithAppendedAttributes(ctx context.Context, toAppend []attribute.KeyValue) context.Context {
+	combined := append(attributesFromContext(ctx), toAppend...)
+	return ContextWithAttributes(ctx, combined)
 }
 
 func attributesFromContext(ctx context.Context) []attribute.KeyValue {
