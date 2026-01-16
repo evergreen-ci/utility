d4784568eee902d2b0749e0c1d5e71728879a66d
chaya <malikchaya2@gmail.com>
2026-01-16 11:36:23 -0500

    export

--- conversion.go
+++ conversion.go
@@ -2,8 +2,8 @@ package utility
 
 // This file includes type conversion functions.
 
-// convertInt32PtrToIntPtr converts *int32 to *int.
-func convertInt32PtrToIntPtr(i32 *int32) *int {
+// ConvertInt32PtrToIntPtr converts *int32 to *int.
+func ConvertInt32PtrToIntPtr(i32 *int32) *int {
 	if i32 == nil {
 		return nil
 	}
